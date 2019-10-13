c65dc114033a57a8c69f0b18adab4d5751fe076e
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-13 10:38:44 +0100

    initial commit - prepared files architecture

---
+++ src/layouts/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <p>u-lala</p>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
