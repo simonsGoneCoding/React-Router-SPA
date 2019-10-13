f0e7a0a162920687bcfef983920a0763c6d96b8c
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-13 12:01:38 +0100

    added layout components to main one and some CSS flexbox styling

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import "../styles/App.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Navigation from "./Navigation";
+import Page from "./Page";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <p>u-lala</p>
+        <div className="app">
+          <header>{<Header />}</header>
+          <main>
+            <aside>{<Navigation />}</aside>
+            <section className="page">{<Page />}</section>
+          </main>
+          <footer>{<Footer />}</footer>
         </div>
       </Router>
     );
---
+++ src/layouts/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <p>Navi</p>
+    </div>
+  );
+};
+
+export default Navigation;
