56af0f495e0ce62a218d880edb094ce2c5e95d2c
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-13 12:50:57 +0100

    updated Navigation component together with CSS styling file

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Navigation.css";
+
+const list = [
+  { name: "Start", path: "/", exact: true },
+  { name: "Products", path: "/products" },
+  { name: "Contact", path: "/contact" },
+  { name: "Admin Panel", path: "/admin" }
+];
 
 const Navigation = () => {
+  const menu = list.map(item => (
+    <li key={item.name}>
+      <NavLink to={item.path} exact={item.exact ? item.exact : false}>
+        {item.name}
+      </NavLink>
+    </li>
+  ));
+
   return (
-    <div>
-      <p>Navi</p>
-    </div>
+    <nav className="main">
+      <ul>{menu}</ul>
+    </nav>
   );
 };
 
