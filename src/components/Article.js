d9f9cffcccbe925821b9431525c10f8f38f4e33a
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-14 21:02:56 +0100

    added some CSS styling to Article component

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,10 +2,18 @@ import React from "react";
 
 const Article = ({ title, author, text }) => {
   // const { title, author, text } = props;  <-- same thing if given as a props argument
+
+  const styles = {
+    marginTop: 40
+  };
   return (
-    <article>
-      <h3>Title: {title} </h3>
-      <span>Author: {author}</span>
+    <article style={styles}>
+      <h3 style={{ marginBottom: 3, textTransform: "uppercase" }}>
+        Title: {title}
+      </h3>
+      <span style={{ display: "block", marginBottom: 10, fontSize: 13 }}>
+        Author: {author}
+      </span>
       <p>{text}</p>
     </article>
   );
