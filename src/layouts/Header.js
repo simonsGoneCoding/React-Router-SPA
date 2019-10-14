f89644b19258b80cabf4167b5b5d695c6f024f21
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-14 18:28:04 +0100

    updated Header component together with CSS styling

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "../styles/Header.css";
+import img1 from "../images/header1.jpg";
+import img2 from "../images/header2.jpg";
+import img3 from "../images/header3.jpg";
 
 const Header = () => {
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <>
+      <Switch>
+        <Route path="/products" render={() => <img src={img1} alt="city" />} />
+        <Route path="/contact" render={() => <img src={img2} alt="city" />} />
+        <Route path="/admin" render={() => <img src={img3} alt="city" />} />
+        <Route path="/" exact render={() => <img src={img2} alt="city" />} />
+        <Route render={() => <img src={img1} alt="city" />} />
+      </Switch>
+    </>
   );
 };
 
