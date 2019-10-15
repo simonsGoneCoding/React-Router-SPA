d185b1bd5a0b6480ba0f32a475c3943cc213668a
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-15 19:05:20 +0100

    created working <form> in ContactPage component

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,39 @@
 import React from "react";
+import "../styles/ContactPage.css";
 
-const ContactPage = () => {
-  return <div>ualalalal</div>;
-};
+class ContactPage extends React.Component {
+  state = {
+    value: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      value: ""
+    });
+  };
+
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Pop us a message!</h3>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Write here"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
