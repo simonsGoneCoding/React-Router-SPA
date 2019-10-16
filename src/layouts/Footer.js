10ad21d0bbf453b7b2884aed4c86416284fbcf33
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-16 18:54:54 +0100

    footer updated

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import "../styles/Footer.css";
 
 const Footer = () => {
   return (
     <div>
-      <p>Footer</p>
+      <h2>Footer</h2>
+      <Route
+        path="/"
+        exact
+        render={() => (
+          <p>
+            You're in the <span>main</span> content of this page{" "}
+          </p>
+        )}
+      />
+
+      <Route
+        path="/:page"
+        // exact
+        render={props => {
+          console.log(props);
+          return (
+            <p>
+              You're in the <span>{props.match.params.page}</span> content of
+              this page
+            </p>
+          );
+        }}
+      />
     </div>
   );
 };
